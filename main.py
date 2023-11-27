052f8512caedfeb15065294911cefa81f5d34462
JoPanta <jopantaleao@gmail.com>
2023-11-27 19:47:20 +0000

    first implementation

---
+++ main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Arkanoid")
+screen.tracer(0)
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
